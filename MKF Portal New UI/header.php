4193f5d80e007ad8f2cbb7f353f559b37e17468c
apecdw <125763225+apecdw@users.noreply.github.com>
2023-04-03 21:39:55 +0000

    Add files via upload

---
+++ MKF Portal New UI/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    
+    <!--Page Title-->
+    <title>MasterKey Portal</title>
+	<meta content="" name="description">
+	<meta content="" name="keywords">
+
+    <!--Page Icon-->
+    <link rel="icon" type="image/x-icon" href="assets/img/MKFLogo.png">
+
+    <!--Icons Library-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!--Fonts (from Google Fonts)-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;600;900&display=swap" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+	<link href="assets/vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+	<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+	<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+	<link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+
+    <!--CSS Linkage-->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div class="sidebar">
+        <a class="active" href="#dashboard"><i class="fa-solid fa-table-list"></i>  Dashboard</a>
+        <a href="#wallet"><i class="fa-solid fa-wallet"></i>  Wallet</a>
+        <a href="#bounties"><i class="fa-solid fa-money-check-dollar"></i> Bounties</a>
+        <a href="#settings"><i class="fa-solid fa-gear"></i> Settings</a>
+      </div>
+
+    <div class="header">
+        <h1><img src="assets/img/MasterKey Portal.png" style="width: 250px; height: 84px; margin-top: -16px; margin-left: 10px;"></h1>
+    </div>
+
+</body>
